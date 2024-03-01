71e7b4a780349320ae967f6be860d6aa5d86dd0f
elisawij <159451658+elisawij@users.noreply.github.com>
2024-03-01 20:25:40 +0100

    Add files via upload

--- js/img-loop.js
+++ js/img-loop.js
@@ -1,22 +1,34 @@
-var loop;  // Declare it on global scope.
+var loop // Declare it on global scope.
 
 $('img.img-loop')
-    .mouseover(function() {
-    $(this).data('old-src', $(this).attr('src'));
-    var alt_src = $(this).data('alt-src').split(';');
-    var that = $(this);
-    var i=0;
-    loop = setInterval(function(){  // Set an interval
-        if(i==alt_src.length){
-            i=0;
-            that.attr("src", that.data('old-src'));
-        }else{
-            that.attr('src', alt_src[i]);
-            i++;
-        }
-    },600);  // Interval delay in millisecs.
+  .mouseover(function () {
+    $(this).data('old-src', $(this).attr('src'))
+    var alt_src = $(this).data('alt-src').split(';')
+    var that = $(this)
+    var i = 0
+    loop = setInterval(function () {
+      // Set an interval
+      if (i == alt_src.length) {
+        i = 0
+        that.attr('src', that.data('old-src'))
+      } else {
+        that.attr('src', alt_src[i])
+        i++
+      }
+    }, 600) // Interval delay in millisecs.
+  })
+  .mouseout(function () {
+    clearInterval(loop) // Clear the interval
+    $(this).attr('src', $(this).data('old-src'))
+  })
+
+$(document).ready(function () {
+  $('button#about').click(function () {
+    $('.bio').toggle()
+    $('.contact').hide()
+  })
+  $('button#contact').click(function () {
+    $('.contact').toggle()
+    $('.bio').hide()
+  })
 })
-    .mouseout(function() {
-    clearInterval(loop);  // Clear the interval
-    $(this).attr('src', $(this).data('old-src'));
-});
