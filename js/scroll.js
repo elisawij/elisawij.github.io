476b555df5194d4656d2022878a242ee5efa4d1a
elisawij <159451658+elisawij@users.noreply.github.com>
2024-03-22 00:56:32 +0100

    Add files via upload

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,4 @@
-const mediaQuery = '(min-width: 900px)'
+const mediaQuery = '(min-width: 800px)'
 const mediaQueryMatch = window.matchMedia(mediaQuery)
 const imageWrapper = $('.image-wrapper')
 
@@ -11,25 +11,48 @@ function HorScroll(vertscroll) {
       vertscroll.originalEvent.deltaX
   )
 }
+// function to check if a color is dark
+function isDark(color) {
+  var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color)
+  return (
+    parseFloat(match[1]) + parseFloat(match[2]) + parseFloat(match[3]) <
+    3 * (256 / 2)
+  ) // r+g+b should be less than half of max (3 * 256)
+}
+
+// function to change content & color of specified elements based on current div.kolom (vertical & horizontal)
+function changeContent() {
+  // Find the corresponding hidden p element for this div.kolom & get it's color
+  var hiddenP = $(this).find('.hidden')
+  var scrollColor = hiddenP.css('color')
+
+  // Display the content of p.hidden in .img-info
+  $('.img-info').html(hiddenP.html())
+
+  // Change the color of selected elements accordingly
+  $('header, .nav, button, footer').css('background-color', scrollColor)
+  $('.site-title, .img-info, button').css(
+    'color',
+    // Change text color based on darkness of background color
+    isDark($('header').css('background-color')) ? 'white' : 'black'
+  )
+  $('#colorWord').css('color', scrollColor)
+}
+
+// extra functionalities for horizontal layout
+function changeContentHover() {
+  changeContent.call($(this))
+  $('.nav, button').css('background-color', 'whitesmoke')
+  $('button').css('color', 'black')
+}
+
 // Function to show p's for each div hovered (horizontal layout)
 function pHover() {
-  // Add event listener for each "kolom" element
-  $('.kolom').hover(
-    function () {
-      // todo: functie isoleren, definiëren & hergebruiken hier + in pScroll
-      // Find the corresponding hidden p element
-      var hiddenP = $(this).find('.hidden')
-      var color = hiddenP.attr('style')
-
-      // Display the content and style of the hidden p in .img-info
-      $('.img-info').html(hiddenP.html()).attr('style', color)
-      $('h1').attr('style', color)
-    },
-    function () {
-      // Clear .img-info when mouse leaves
-      $('.img-info').empty().removeAttr('style')
-    }
-  )
+  // Add event listener for each div.kolom
+  $('.kolom').hover(changeContentHover, function () {
+    // Clear .img-info when mouse leaves
+    // $('.img-info').empty().removeAttr('style')
+  })
 }
 
 // Function to check if an element overlaps the vertical center of the viewport (vertical layout)
@@ -41,16 +64,10 @@ function isInViewport(element) {
 }
 // Function to show p's for each div passing the center of the viewport (vertical layout)
 function pScroll() {
-  // Check if each "kolom" element is in the viewport and update .img-info
+  // Check if each "kolom" element is in the viewport and update the content
   $('.kolom').each(function () {
     if (isInViewport($(this))) {
-      // Find the corresponding hidden p element
-      var hiddenP = $(this).find('.hidden')
-      var color = hiddenP.attr('style')
-
-      // Display the content and style of the hidden p in .img-info
-      $('.img-info').html(hiddenP.html()).attr('style', color)
-      $('h1').attr('style', color)
+      changeContent.call($(this))
     }
   })
 }
@@ -59,6 +76,20 @@ function pScroll() {
 // horizontal
 if (mediaQueryMatch.matches) {
   imageWrapper.on('wheel', HorScroll)
+
+  // changeContentHover but with different variable
+  var firstHiddenP = $('.kolom:first-child .hidden')
+  $('.img-info').html(firstHiddenP.html())
+  var scrollColor = firstHiddenP.css('color')
+  $('header, .nav, button, footer').css('background-color', scrollColor)
+  $('.site-title, .img-info, button').css(
+    'color',
+    isDark($('header').css('background-color')) ? 'white' : 'black'
+  )
+  $('#colorWord').css('color', scrollColor)
+  $('.nav, button').css('background-color', 'whitesmoke')
+  $('button').css('color', 'black')
+
   pHover()
 }
 // vertical
@@ -73,7 +104,8 @@ mediaQueryMatch.onchange = (event) => {
   if (event.matches) {
     $('.image-container').off()
     $('.img-info').empty().removeAttr('style')
-    $('h1').removeAttr('style')
+    $('h1, button').removeAttr('style')
+    $('.nav').css('background-color', 'whitesmoke')
 
     imageWrapper.on('wheel', HorScroll)
     pHover()
